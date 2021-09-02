46f66517a238b356ac75d7fbe6cda653796cdd17
JerushaaJane <67820488+JerushaaJane@users.noreply.github.com>
2021-09-02 14:26:13 +0530

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int n=1634;
+      int sum=0,r,temp;
+      temp=n;
+      while(n>0){
+          r=n%10;
+          sum=sum+(r*r*r*r);
+          n=n/10;
+      }
+      if(temp==sum){System.out.println("Armstrong");}
+      else{System.out.println("Non Armstrong");}
+    }
+}
