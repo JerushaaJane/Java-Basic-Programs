0cfc6e33c73b5e9c250b7492c155ed1f365ef22c
JerushaaJane <67820488+JerushaaJane@users.noreply.github.com>
2021-09-16 20:55:55 +0530

    Create NumberReverse.java

---
+++ NumberReverse.java
@@ -0,0 +1,11 @@
+public class NumberReverse {
+    public static void main(String[] args) {
+      int n=144,r,sum=0;
+      while(n>0){
+        r=n%10;
+        sum=(sum*10)+r;
+        n=n/10;
+      }
+      System.out.println(sum);
+    }
+}
